f73abc88cec8d95928fe78361bac725bf104f853
Shams_Keshk <shams.keshk@gmail.com>
2022-02-06 03:26:43 +0200

    - Split WorkInfo Extentions from Learning model to WorkManager extentions .

--- app/src/main/java/com/example/liteeducation/data/extentions/WorkManagerExtentions.kt
+++ app/src/main/java/com/example/liteeducation/data/extentions/WorkManagerExtentions.kt
@@ -1,9 +1,31 @@
 package com.example.liteeducation.data.extentions
 
+import androidx.work.Data
 import androidx.work.WorkInfo
+import com.example.liteeducation.data.model.LearningMaterial
 import com.example.liteeducation.data.remote.services.DownloadWorkManager
-
+import com.squareup.moshi.Moshi
 
 fun WorkInfo.getProgressAsInt(): Int {
-    return this.progress.getInt(DownloadWorkManager.Progress, 0)
+    return this.progress.getInt(DownloadWorkManager.Progress, -1)
+}
+
+// Serialize a single object.
+fun Data.Builder.putLearningMaterial(key: String, learningMaterial: LearningMaterial) {
+    val moshi = Moshi.Builder().build().adapter(LearningMaterial::class.java).lenient()
+    this.putString(key,moshi.toJson(learningMaterial))
+}
+
+// Deserialize to single object.
+fun Data.getLearningMaterial(jsonKey: String): LearningMaterial? {
+    val moshi = Moshi.Builder().build()
+        .adapter(LearningMaterial::class.java).lenient()
+
+    val value = this.getString(jsonKey)
+
+    if(value != null) {
+        return moshi.fromJson(value)
+    }
+
+    return null
 }
--- app/src/main/java/com/example/liteeducation/data/model/LearningMaterial.kt
+++ app/src/main/java/com/example/liteeducation/data/model/LearningMaterial.kt
@@ -1,15 +1,15 @@
 package com.example.liteeducation.data.model
 
+import androidx.work.Data
 import com.example.liteeducation.R
+import com.squareup.moshi.Moshi
 
 data class LearningMaterial(var id: Int,
                             var type: String,
                             var url: String,
                             var name: String,
                             @Transient
-                            var downloadState: Result<String>?) {
-
-}
+                            var downloadState: Result<String>?)
 
 fun LearningMaterial.isTypeVideo() : Boolean{
     return this.type == MaterialTypes.VIDEO.name
@@ -19,23 +19,8 @@ fun LearningMaterial.isTypePDF() : Boolean{
     return this.type == MaterialTypes.PDF.name
 }
 
-
-enum class MaterialTypes {
-    VIDEO , PDF
+fun LearningMaterial.getDownloadProgress(): Int = when(downloadState){
+    is Result.LoadingProgress -> (downloadState as Result.LoadingProgress<String>).progress
+    else -> -1
 }
 
-class LearningMaterialFactory{
-
-    companion object {
-
-        fun getImageResourceFor(learningMaterial: LearningMaterial) : Int{
-            return if (learningMaterial.isTypeVideo()){
-                R.drawable.ic_ondemand_video_60
-            }else if (learningMaterial.isTypePDF()){
-                R.drawable.ic_pdf_60
-            }else{
-                R.drawable.ic_un_known_type_60
-            }
-        }
-    }
-}
